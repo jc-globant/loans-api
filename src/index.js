1e22c6ce778e2ccc864a985149343eb4c63df4a2
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2024-05-10 00:37:02 -0600

    database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
-const express = require('express')
+import express from 'express'
+import { db } from './db/index.js'
+
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api/loans/:id', (req, res) => {
+  console.log(req.params.id)
+  try {
+    const query = `
+    SELECT
+      L.id AS loanId,
+      L.clientId,
+      C.name AS clientName,
+      L.loanAmount,
+      L.monthlyPayments,
+      L.referral,
+      P.paymentDate,
+      P.amountPaid,
+      P.status
+    FROM
+        Loans AS L
+    JOIN
+        Clients AS C ON L.clientId = C.id
+    LEFT JOIN
+        Payments AS P ON L.id = P.loanId
+    WHERE
+        L.id = ?;
+`
+
+    const stmt = db.prepare(query)
+    const result = stmt.get(req.params.id)
+
+    res.status(200).json({ result })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({ result: 'error' })
+  }
 })
 
 app.listen(port, () => {

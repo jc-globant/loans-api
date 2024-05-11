bcfd7b570aee9cd1d4275b563e067dde6c65b260
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2024-05-10 19:18:47 -0600

    server.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,5 @@
-import express from 'express';
-import { db } from './db/index.js';
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/api/loans/:id', (req, res) => {
-  console.log(req.params.id);
-  try {
-    const query = `
-    SELECT
-      L.id AS loanId,
-      L.clientId,
-      C.name AS clientName,
-      L.loanAmount,
-      L.monthlyPayments,
-      L.referral,
-      P.paymentDate,
-      P.amountPaid,
-      P.status
-    FROM
-        Loans AS L
-    JOIN
-        Clients AS C ON L.clientId = C.id
-    LEFT JOIN
-        Payments AS P ON L.id = P.loanId
-    WHERE
-        L.id = ?;
-`;
-
-    const stmt = db.prepare(query);
-    const result = stmt.get(req.params.id);
-
-    res.status(200).json({ result });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ result: 'error' });
-  }
-});
+import { app, port } from './server.js';
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port} 🚀`);
 });
---
+++ src/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { db } from './db/index.js';
+
+export const app = express();
+export const port = process.env.PORT || 3000;
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api/loans/:id', (req, res) => {
+  console.log(req.params.id);
+  try {
+    const query = `
+    SELECT
+      L.id AS loanId,
+      L.clientId,
+      C.name AS clientName,
+      L.loanAmount,
+      L.monthlyPayments,
+      L.referral,
+      P.paymentDate,
+      P.amountPaid,
+      P.status
+    FROM
+        Loans AS L
+    JOIN
+        Clients AS C ON L.clientId = C.id
+    LEFT JOIN
+        Payments AS P ON L.id = P.loanId
+    WHERE
+        L.id = ?;
+`;
+
+    const stmt = db.prepare(query);
+    const result = stmt.get(req.params.id);
+
+    res.status(200).json({ result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ result: 'error' });
+  }
+});

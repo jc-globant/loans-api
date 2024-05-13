b4178c61d09fd8901e0a50b8707a1c1af9247f49
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2024-05-12 21:48:22 -0600

    client validation

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -1,6 +1,16 @@
 import { Router } from 'express';
+import { check } from 'express-validator';
 import { controller } from '../controllers/client.controller.js';
+import { validateFields } from '../middlewares/validate-fields.js';
 
 export const clientController = Router();
 
-clientController.post('/clients', controller.create);
+clientController.post(
+  '/clients',
+  [
+    check('name', 'Name is required').notEmpty(),
+    check('phone', 'Phone not valid').isNumeric().isLength({ min: 7 }),
+    validateFields,
+  ],
+  controller.create,
+);

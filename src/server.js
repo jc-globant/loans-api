20ec89dcb4dc7031696d82840ff0457679d83053
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2024-05-13 16:17:52 -0600

    static

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 import { routes } from './routes/index.js'
 
 export const server = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 server.use(morgan('tiny'))
 server.use(cors())
@@ -11,6 +16,8 @@ server.use(express.json())
 
 server.use(routes)
 
-server.get('/', (req, res) => {
+server.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
+
+server.use('/', express.static(path.join(__dirname, 'public')))

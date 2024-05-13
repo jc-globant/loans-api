51bf204267c885423d81de6cee9c7f4eb21d982a
Jorge Calderón Peralta <76452947+calderonjorge@users.noreply.github.com>
2024-05-12 21:37:39 -0600

    middlewares

---
+++ src/middlewares/validate-jwt.js
@@ -0,0 +1,26 @@
+import { response, request } from 'express';
+import jwt from 'jsonwebtoken';
+import { config } from '../config';
+
+export const validateJWT = (req = request, res = response, next) => {
+  const token = req.headers('x-token');
+
+  if (!token) {
+    return res.status(401).json({
+      ok: false,
+      msg: 'Token not found',
+    });
+  }
+
+  try {
+    jwt.verify(token, config.SECRET_JWT_SEED);
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({
+      ok: false,
+      msg: 'Invalid token',
+    });
+  } finally {
+    next();
+  }
+};
